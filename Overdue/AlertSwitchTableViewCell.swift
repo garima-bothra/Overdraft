ed05a08410e793d15a959f456fadc9a6905f10a2
garima94921 <gaarimabothra@gmail.com>
2020-05-04 19:34:38 +0530

    Tableview setup added

---
+++ Overdue/AlertSwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlertSwitchTableViewCell.swift
+//  Overdue
+//
+//  Created by Garima Bothra on 04/05/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class AlertSwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Overdue/ViewController.swift
+++ Overdue/ViewController.swift
@@ -21,3 +21,30 @@ class OverdraftViewController: UIViewController {
 
 }
 
+ // MARK: - Table view data source
+extension OverdraftViewController: UITableViewDelegate, UITableViewDataSource {
+
+     func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "alertSwitch", for: indexPath) as! AlertSwitchTableViewCell
+            return cell
+        }
+        else {
+          let cell =   tableView.dequeueReusableCell(withIdentifier: "requestIncrease", for: indexPath) as! UITableViewCell
+            return cell
+        }
+
+    }
+
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
