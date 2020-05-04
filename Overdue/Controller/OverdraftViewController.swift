beb458f40447ad9ccb678973492fc7739469397a
garima94921 <gaarimabothra@gmail.com>
2020-05-05 00:27:37 +0530

    UI done

--- Overdue/Controller/OverdraftViewController.swift
+++ Overdue/Controller/OverdraftViewController.swift
@@ -16,11 +16,13 @@ class OverdraftViewController: UIViewController {
     //Creating outlets
     @IBOutlet weak var progressBarView: UIView!
     @IBOutlet weak var interestDisplayView: UIView!
+    @IBOutlet weak var labelsView: UIView!
     @IBOutlet weak var overdraftAlertTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
         setupProgressBar()
+        setupLabelView()
     }
     func setupNavBar() {
         navigationController?.navigationBar.topItem?.title = "OVERDRAFT"
@@ -37,24 +39,40 @@ class OverdraftViewController: UIViewController {
                                         endAngle: CGFloat.pi/4, clockwise: true)
         trackLayer.path = circularPath.cgPath
         trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 35
+        trackLayer.lineWidth = 40
         trackLayer.lineCap = .square
         trackLayer.fillColor = UIColor.clear.cgColor
         limitLayer.path = circularPath.cgPath
         limitLayer.strokeColor = UIColor.white.cgColor
-        limitLayer.lineWidth = 35
+        limitLayer.lineWidth = 40
         limitLayer.strokeEnd = 0.5
         limitLayer.lineCap = .round
         limitLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = UIColor.systemTeal.cgColor
-        shapeLayer.lineWidth = 35
+        shapeLayer.lineWidth = 40
         shapeLayer.strokeEnd = 0.25
         shapeLayer.lineCap = .round
         shapeLayer.fillColor = UIColor.clear.cgColor
         progressBarView.layer.addSublayer(trackLayer)
         progressBarView.layer.addSublayer(limitLayer)
         progressBarView.layer.addSublayer(shapeLayer)
+
+    }
+
+    func setupLabelView() {
+        let lineLayer = CAShapeLayer()
+        let linePath = UIBezierPath()
+        var midY = labelsView.frame.size.width/2
+        var midX = labelsView.frame.size.height/2
+        var center: CGPoint { return CGPoint(x: midX, y: midY) }
+        linePath.move(to: CGPoint(x: center.x - 50, y: center.y))
+        linePath.addLine(to: CGPoint(x: center.x + 50, y: center.y))
+     //   linePath.close()
+        lineLayer.path = linePath.cgPath
+        lineLayer.lineWidth = 5
+        lineLayer.fillColor = UIColor.darkGray.cgColor
+        labelsView.layer.addSublayer(lineLayer)
 
     }
 
